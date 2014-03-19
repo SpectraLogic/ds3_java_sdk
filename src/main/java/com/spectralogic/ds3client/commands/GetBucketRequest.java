29b6132d4d82d5afbd0c30395df167dca98d9252
Ryan Moore <ryan.moore@greyrocksoft.com>
2014-03-19 16:22:15 -0600

    Adding in support to do pagination when getting all the objects in a bucket.

--- src/main/java/com/spectralogic/ds3client/commands/GetBucketRequest.java
+++ src/main/java/com/spectralogic/ds3client/commands/GetBucketRequest.java
@@ -6,9 +6,31 @@ import com.spectralogic.ds3client.HttpVerb;
 public class GetBucketRequest extends AbstractRequest {
 
     final private String bucket;
+    private String nextMarker = null;
+    private String prefix = null;
 
     public GetBucketRequest(final String bucket) {
         this.bucket = bucket;
+    }
+
+    public GetBucketRequest withNextMarker(final String nextMarker) {
+        this.nextMarker = nextMarker;
+        this.getQueryParams().put("marker", nextMarker);
+        return this;
+    }
+
+    public GetBucketRequest withPrefix(final String prefix) {
+        this.prefix = prefix;
+        this.getQueryParams().put("prefix", prefix);
+        return this;
+    }
+
+    public String getNextMarker() {
+        return nextMarker;
+    }
+
+    public String getPrefix() {
+        return prefix;
     }
 
     @Override
@@ -20,4 +42,6 @@ public class GetBucketRequest extends AbstractRequest {
     public HttpVerb getVerb() {
         return HttpVerb.GET;
     }
+
+
 }
