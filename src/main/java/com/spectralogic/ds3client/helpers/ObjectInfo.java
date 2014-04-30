79527d20f7a6fa0fd509cadceb0a93a706ae2de4
Hans-Peter Klett <hansk@spectralogic.com>
2014-04-30 15:56:46 -0600

    Made ObjectInfo package private.

--- src/main/java/com/spectralogic/ds3client/helpers/ObjectInfo.java
+++ src/main/java/com/spectralogic/ds3client/helpers/ObjectInfo.java
@@ -2,7 +2,7 @@ package com.spectralogic.ds3client.helpers;
 
 import com.spectralogic.ds3client.models.Ds3Object;
 
-public interface ObjectInfo {
+interface ObjectInfo {
     public String getKey();
     public Ds3Object getObject();
 }
