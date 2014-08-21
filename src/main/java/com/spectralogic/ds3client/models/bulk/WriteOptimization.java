5510812c4a427c9f761c8c7e7e2db858cc1a0b33
Ryan Moore <ryan.moore@greyrocksoft.com>
2014-08-21 17:14:56 -0600

    Updating the new enums to the latest values and adding a valuesString function to print out all the values for the enum.

--- src/main/java/com/spectralogic/ds3client/models/bulk/WriteOptimization.java
+++ src/main/java/com/spectralogic/ds3client/models/bulk/WriteOptimization.java
@@ -1,5 +1,21 @@
 package com.spectralogic.ds3client.models.bulk;
 
+import com.google.common.base.Function;
+import com.google.common.base.Joiner;
+import com.google.common.collect.Lists;
+
+import java.util.ArrayList;
+
 public enum WriteOptimization {
-    CAPACITY, PERFORMANCE
+    CAPACITY, PERFORMANCE;
+	
+	    public static String valuesString() {
+        final ArrayList<WriteOptimization> list = Lists.newArrayList(WriteOptimization.values());
+        return Joiner.on(", ").join(Lists.transform(list, new Function<WriteOptimization, String>() {
+            @Override
+            public String apply(final WriteOptimization input) {
+                return input.toString().toLowerCase();
+            }
+        }));
+    }
 }
