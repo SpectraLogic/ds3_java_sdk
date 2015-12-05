93d3d9eccabcded36055d8f648e39f88543d8efc
Ryan Moore <ryan.moore@greyrocksoft.com>
2015-12-05 13:47:51 -0700

    Made sure there were negative and positive cases for both guard methods

--- ds3-sdk/src/test/java/com/spectralogic/ds3client/utils/Guard_Test.java
+++ ds3-sdk/src/test/java/com/spectralogic/ds3client/utils/Guard_Test.java
@@ -3,6 +3,8 @@ package com.spectralogic.ds3client.utils;
 import com.google.common.collect.Lists;
 import org.junit.Test;
 
+import java.util.List;
+
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
@@ -11,15 +13,20 @@ public class Guard_Test {
     @Test
     public void testCollectionNull() {
         assertTrue(Guard.isNullOrEmpty(null));
+        assertFalse(Guard.isNotNullAndNotEmpty(null));
     }
 
     @Test
     public void testCollectionEmpty() {
-        assertTrue(Guard.isNullOrEmpty(Lists.newArrayList()));
+        final List<String> data = Lists.newArrayList();
+        assertTrue(Guard.isNullOrEmpty(data));
+        assertFalse(Guard.isNotNullAndNotEmpty(data));
     }
 
     @Test
     public void testNotEmptyCollection() {
-        assertFalse(Guard.isNullOrEmpty(Lists.newArrayList("hi")));
+        final List<String> data = Lists.newArrayList("hi");
+        assertFalse(Guard.isNullOrEmpty(data));
+        assertTrue(Guard.isNotNullAndNotEmpty(data));
     }
 }
