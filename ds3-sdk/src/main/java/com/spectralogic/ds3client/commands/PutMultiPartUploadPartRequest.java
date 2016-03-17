692f6bbf5d920c0a53013075c9fed6872d2f8bf7
Rachel Tucker <rachelt@spectralogic.com>
2016-03-17 16:38:36 -0600

    Added request payload to PutMultiPartUploadPartRequest

--- ds3-sdk/src/main/java/com/spectralogic/ds3client/commands/PutMultiPartUploadPartRequest.java
+++ ds3-sdk/src/main/java/com/spectralogic/ds3client/commands/PutMultiPartUploadPartRequest.java
@@ -19,11 +19,14 @@ package com.spectralogic.ds3client.commands;
 import com.spectralogic.ds3client.networking.HttpVerb;
 import java.util.UUID;
 import com.google.common.net.UrlEscapers;
+import com.spectralogic.ds3client.utils.SeekableByteChannelInputStream;
+import java.nio.channels.SeekableByteChannel;
+import java.io.InputStream;
 
 public class PutMultiPartUploadPartRequest extends AbstractRequest {
 
     // Variables
-    
+
     private final String bucketName;
 
     private final String objectName;
@@ -32,24 +35,60 @@ public class PutMultiPartUploadPartRequest extends AbstractRequest {
 
     private final String uploadId;
 
+    private final long size;
+
+    private final InputStream stream;
+
+    private SeekableByteChannel channel;
+
     // Constructor
-    
-    public PutMultiPartUploadPartRequest(final String bucketName, final String objectName, final int partNumber, final UUID uploadId) {
+
+    public PutMultiPartUploadPartRequest(final String bucketName, final String objectName, final SeekableByteChannel channel, final int partNumber, final long size, final UUID uploadId) {
         this.bucketName = bucketName;
         this.objectName = objectName;
         this.partNumber = partNumber;
         this.uploadId = uploadId.toString();
-        
+        this.size = size;
+        this.channel = channel;
+
         this.getQueryParams().put("part_number", Integer.toString(partNumber));
         this.getQueryParams().put("upload_id", uploadId.toString());
+        this.stream = new SeekableByteChannelInputStream(channel);
     }
 
-    public PutMultiPartUploadPartRequest(final String bucketName, final String objectName, final int partNumber, final String uploadId) {
+    public PutMultiPartUploadPartRequest(final String bucketName, final String objectName, final SeekableByteChannel channel, final int partNumber, final long size, final String uploadId) {
         this.bucketName = bucketName;
         this.objectName = objectName;
         this.partNumber = partNumber;
         this.uploadId = uploadId;
-        
+        this.size = size;
+        this.channel = channel;
+
+        this.getQueryParams().put("part_number", Integer.toString(partNumber));
+        this.getQueryParams().put("upload_id", UrlEscapers.urlFragmentEscaper().escape(uploadId).replace("+", "%2B"));
+        this.stream = new SeekableByteChannelInputStream(channel);
+    }
+
+    public PutMultiPartUploadPartRequest(final String bucketName, final String objectName, final int partNumber, final long size, final InputStream stream, final UUID uploadId) {
+        this.bucketName = bucketName;
+        this.objectName = objectName;
+        this.partNumber = partNumber;
+        this.uploadId = uploadId.toString();
+        this.size = size;
+        this.stream = stream;
+
+        this.getQueryParams().put("part_number", Integer.toString(partNumber));
+        this.getQueryParams().put("upload_id", uploadId.toString());
+    }
+
+    public PutMultiPartUploadPartRequest(final String bucketName, final String objectName, final int partNumber, final long size, final InputStream stream, final String uploadId) {
+        this.bucketName = bucketName;
+        this.objectName = objectName;
+        this.partNumber = partNumber;
+        this.uploadId = uploadId;
+        this.size = size;
+        this.stream = stream;
+
         this.getQueryParams().put("part_number", Integer.toString(partNumber));
         this.getQueryParams().put("upload_id", UrlEscapers.urlFragmentEscaper().escape(uploadId).replace("+", "%2B"));
     }
@@ -64,7 +103,7 @@ public class PutMultiPartUploadPartRequest extends AbstractRequest {
     public String getPath() {
         return "/" + this.bucketName + "/" + this.objectName;
     }
-    
+
     public String getBucketName() {
         return this.bucketName;
     }
@@ -82,6 +121,21 @@ public class PutMultiPartUploadPartRequest extends AbstractRequest {
 
     public String getUploadId() {
         return this.uploadId;
+    }
+
+
+    public long getSize() {
+        return this.size;
+    }
+
+
+    public InputStream getStream() {
+        return this.stream;
+    }
+
+
+    public SeekableByteChannel getChannel() {
+        return this.channel;
     }
 
 }
