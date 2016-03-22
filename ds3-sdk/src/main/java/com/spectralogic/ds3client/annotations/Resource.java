617764114a5ae8bc25fe97340aee3e86c0809b82
Rachel Tucker <rachelt@spectralogic.com>
2016-03-22 15:35:05 -0600

    Removed auto-gened comment from annotation files

--- ds3-sdk/src/main/java/com/spectralogic/ds3client/annotations/Resource.java
+++ ds3-sdk/src/main/java/com/spectralogic/ds3client/annotations/Resource.java
@@ -13,8 +13,6 @@
  * ****************************************************************************
  */
 
-// This code is auto-generated, do not modify
-
 package com.spectralogic.ds3client.annotations;
 
 import java.lang.annotation.ElementType;
