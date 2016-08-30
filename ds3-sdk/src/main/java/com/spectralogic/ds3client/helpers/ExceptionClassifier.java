0709e55522492be5d2ff5cf63fe152c7aa4431f1
GraciesPadre <ShastasPadre1@gmail.com>
2016-08-30 16:17:13 -0600

    Adding FailedRequestException as one we can recover from when retrying put object requests

--- ds3-sdk/src/main/java/com/spectralogic/ds3client/helpers/ExceptionClassifier.java
+++ ds3-sdk/src/main/java/com/spectralogic/ds3client/helpers/ExceptionClassifier.java
@@ -1,12 +1,14 @@
 package com.spectralogic.ds3client.helpers;
 
 import com.spectralogic.ds3client.exceptions.Ds3NoMoreRetriesException;
+import com.spectralogic.ds3client.networking.FailedRequestException;
 
 import java.io.IOException;
 
 public class ExceptionClassifier {
     public static boolean isRecoverableException(final Throwable t) {
         if(
+                t instanceof FailedRequestException    ||
                 t instanceof Ds3NoMoreRetriesException ||
                 t instanceof IOException
         )
