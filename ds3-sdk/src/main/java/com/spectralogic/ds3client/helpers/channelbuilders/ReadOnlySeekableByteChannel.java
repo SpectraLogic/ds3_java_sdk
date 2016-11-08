40b09535748164ca740f2cd60c640e8a368c679e
Rachel Tucker <rachelt@spectralogic.com>
2016-11-07 17:09:28 -0700

    Added overload to tranfer method with stream implementation and unit tests

---
+++ ds3-sdk/src/main/java/com/spectralogic/ds3client/helpers/channelbuilders/ReadOnlySeekableByteChannel.java
@@ -0,0 +1,76 @@
+/*
+ * ****************************************************************************
+ *    Copyright 2014-2016 Spectra Logic Corporation. All Rights Reserved.
+ *    Licensed under the Apache License, Version 2.0 (the "License"). You may not use
+ *    this file except in compliance with the License. A copy of the License is located at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    or in the "license" file accompanying this file.
+ *    This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ *    CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ *    specific language governing permissions and limitations under the License.
+ *  ****************************************************************************
+ */
+
+package com.spectralogic.ds3client.helpers.channelbuilders;
+
+import com.spectralogic.ds3client.utils.NotImplementedException;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.ReadableByteChannel;
+import java.nio.channels.SeekableByteChannel;
+
+public class ReadOnlySeekableByteChannel implements SeekableByteChannel {
+
+    private final ReadableByteChannel channel;
+
+    public ReadOnlySeekableByteChannel(final ReadableByteChannel channel) {
+        this.channel = channel;
+    }
+
+    public ReadableByteChannel getChannel() {
+        return channel;
+    }
+
+    @Override
+    public int read(final ByteBuffer dst) throws IOException {
+        return channel.read(dst);
+    }
+
+    @Override
+    public int write(ByteBuffer src) throws IOException {
+        throw new NotImplementedException();
+    }
+
+    @Override
+    public long position() throws IOException {
+        throw new NotImplementedException();
+    }
+
+    @Override
+    public SeekableByteChannel position(long newPosition) throws IOException {
+        throw new NotImplementedException();
+    }
+
+    @Override
+    public long size() throws IOException {
+        throw new NotImplementedException();
+    }
+
+    @Override
+    public SeekableByteChannel truncate(long size) throws IOException {
+        throw new NotImplementedException();
+    }
+
+    @Override
+    public boolean isOpen() {
+        return channel.isOpen();
+    }
+
+    @Override
+    public void close() throws IOException {
+        channel.close();
+    }
+}
