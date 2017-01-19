7e24aee590b87c53166a635f8b85a9e048862b1f
GraciesPadre <ShastasPadre1@gmail.com>
2017-01-18 18:08:08 -0700

    Fixing Windows unit tests

--- ds3-sdk/src/test/java/com/spectralogic/ds3client/metadata/WindowsMetadataRestore_Test.java
+++ ds3-sdk/src/test/java/com/spectralogic/ds3client/metadata/WindowsMetadataRestore_Test.java
@@ -21,6 +21,8 @@ import com.google.common.collect.Sets;
 import com.spectralogic.ds3client.commands.interfaces.MetadataImpl;
 import com.spectralogic.ds3client.networking.Headers;
 import com.spectralogic.ds3client.networking.Metadata;
+import com.spectralogic.ds3client.utils.ResourceUtils;
+import org.apache.commons.io.FileUtils;
 import org.apache.http.Header;
 import org.apache.http.message.BasicHeader;
 import org.junit.Assert;
@@ -32,6 +34,7 @@ import java.io.File;
 import java.lang.reflect.Method;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.nio.file.attribute.BasicFileAttributes;
 import java.util.List;
 import java.util.Set;
@@ -41,25 +44,28 @@ import static com.spectralogic.ds3client.metadata.MetadataKeyConstants.KEY_OWNER
 import static com.spectralogic.ds3client.metadata.MetadataKeyConstants.METADATA_PREFIX;
 
 public class WindowsMetadataRestore_Test {
+    private static final String FILE_NAME = "LoremIpsum.txt";
+    private static final String localOS = MetaDataUtil.getOS();
+    
     @Before
     public void checkPreconditions() {
         Assume.assumeTrue(org.apache.commons.lang3.SystemUtils.IS_OS_WINDOWS);
     }
-
-    private final File file = new File(getClass().getClassLoader().getResource("LoremIpsum.txt").getFile());
-    private final String localOS = MetaDataUtil.getOS();
-
+    
     @Test
     public void restoreFileTimes_Test() throws Exception {
         final BasicHeader basicHeader[] = new BasicHeader[3];
-        final BasicFileAttributes attr = Files.readAttributes(file.toPath(), BasicFileAttributes.class);
+
+        final Path filePath = ResourceUtils.loadFileResource(FILE_NAME);
+
+        final BasicFileAttributes attr = Files.readAttributes(filePath, BasicFileAttributes.class);
         basicHeader[0] = new BasicHeader(METADATA_PREFIX + MetadataKeyConstants.KEY_CREATION_TIME, String.valueOf(attr.creationTime().toMillis()));
         basicHeader[1] = new BasicHeader(METADATA_PREFIX + MetadataKeyConstants.KEY_ACCESS_TIME, String.valueOf(attr.lastAccessTime().toMillis()));
         basicHeader[2] = new BasicHeader(METADATA_PREFIX + MetadataKeyConstants.KEY_LAST_MODIFIED_TIME, String.valueOf(attr.lastModifiedTime().toMillis()));
         final Metadata metadata = genMetadata(basicHeader);
-        final WindowsMetadataRestore windowsMetadataRestore = new WindowsMetadataRestore(metadata, file.getPath(), MetaDataUtil.getOS());
+        final WindowsMetadataRestore windowsMetadataRestore = new WindowsMetadataRestore(metadata, filePath.toString(), MetaDataUtil.getOS());
         windowsMetadataRestore.restoreFileTimes();
-        final BasicFileAttributes fileAttributes = Files.readAttributes(file.toPath(), BasicFileAttributes.class);
+        final BasicFileAttributes fileAttributes = Files.readAttributes(filePath, BasicFileAttributes.class);
         Assert.assertEquals(String.valueOf(fileAttributes.creationTime().toMillis()), String.valueOf(basicHeader[0].getValue()));
         Assert.assertEquals(String.valueOf(fileAttributes.lastModifiedTime().toMillis()), String.valueOf(basicHeader[2].getValue()));
     }
@@ -67,31 +73,43 @@ public class WindowsMetadataRestore_Test {
 
     @Test
     public void restoreUserAndOwner_Test() throws Exception {
-        final ImmutableMap.Builder<String, String> mMetadataMap = new ImmutableMap.Builder<>();
-        final WindowsMetadataStore windowsMetadataStore = new WindowsMetadataStore(mMetadataMap);
+        final ImmutableMap.Builder<String, String> metadataMap = new ImmutableMap.Builder<>();
+        final WindowsMetadataStore windowsMetadataStore = new WindowsMetadataStore(metadataMap);
         final Class aClass = WindowsMetadataStore.class;
         final Method method = aClass.getDeclaredMethod("saveWindowsDescriptors", new Class[]{Path.class});
         method.setAccessible(true);
-        method.invoke(windowsMetadataStore, file.toPath());
 
-        final BasicHeader basicHeader[] = new BasicHeader[3];
-        basicHeader[0] = new BasicHeader(METADATA_PREFIX + KEY_OWNER, mMetadataMap.build().get(METADATA_PREFIX + KEY_OWNER));
-        basicHeader[1] = new BasicHeader(METADATA_PREFIX + KEY_GROUP, mMetadataMap.build().get(METADATA_PREFIX + KEY_GROUP));
-        basicHeader[2] = new BasicHeader(METADATA_PREFIX + MetadataKeyConstants.KEY_OS, localOS);
-        final Metadata metadata = genMetadata(basicHeader);
-        final WindowsMetadataRestore windowsMetadataRestore = new WindowsMetadataRestore(metadata, file.getPath(), MetaDataUtil.getOS());
-        windowsMetadataRestore.restoreOSName();
-        windowsMetadataRestore.restoreUserAndOwner();
+        final String tempPathPrefix = null;
+        final Path tempDirectory = Files.createTempDirectory(Paths.get("."), tempPathPrefix);
 
-        final ImmutableMap.Builder<String, String> mMetadataMapAfterRestore = new ImmutableMap.Builder<>();
-        final WindowsMetadataStore windowsMetadataStoreAfterRestore = new WindowsMetadataStore(mMetadataMapAfterRestore);
-        final Class aClassAfterRestore = WindowsMetadataStore.class;
-        final Method methodAfterRestore = aClassAfterRestore.getDeclaredMethod("saveWindowsDescriptors", new Class[]{Path.class});
-        methodAfterRestore.setAccessible(true);
-        methodAfterRestore.invoke(windowsMetadataStoreAfterRestore, file.toPath());
+        final String fileName = "Gracie.txt";
 
-        Assert.assertEquals(mMetadataMapAfterRestore.build().get(METADATA_PREFIX + KEY_OWNER), basicHeader[0].getValue());
-        Assert.assertEquals(mMetadataMapAfterRestore.build().get(METADATA_PREFIX + KEY_GROUP), basicHeader[1].getValue());
+        try {
+            final Path filePath = Files.createFile(Paths.get(tempDirectory.toString(), fileName));
+
+            method.invoke(windowsMetadataStore, filePath);
+
+            final BasicHeader basicHeader[] = new BasicHeader[3];
+            basicHeader[0] = new BasicHeader(METADATA_PREFIX + KEY_OWNER, metadataMap.build().get(METADATA_PREFIX + KEY_OWNER));
+            basicHeader[1] = new BasicHeader(METADATA_PREFIX + KEY_GROUP, metadataMap.build().get(METADATA_PREFIX + KEY_GROUP));
+            basicHeader[2] = new BasicHeader(METADATA_PREFIX + MetadataKeyConstants.KEY_OS, localOS);
+            final Metadata metadata = genMetadata(basicHeader);
+            final WindowsMetadataRestore windowsMetadataRestore = new WindowsMetadataRestore(metadata, filePath.toString(), MetaDataUtil.getOS());
+            windowsMetadataRestore.restoreOSName();
+            windowsMetadataRestore.restoreUserAndOwner();
+
+            final ImmutableMap.Builder<String, String> mMetadataMapAfterRestore = new ImmutableMap.Builder<>();
+            final WindowsMetadataStore windowsMetadataStoreAfterRestore = new WindowsMetadataStore(mMetadataMapAfterRestore);
+            final Class aClassAfterRestore = WindowsMetadataStore.class;
+            final Method methodAfterRestore = aClassAfterRestore.getDeclaredMethod("saveWindowsDescriptors", new Class[]{Path.class});
+            methodAfterRestore.setAccessible(true);
+            methodAfterRestore.invoke(windowsMetadataStoreAfterRestore, filePath);
+
+            Assert.assertEquals(mMetadataMapAfterRestore.build().get(METADATA_PREFIX + KEY_OWNER), basicHeader[0].getValue());
+            Assert.assertEquals(mMetadataMapAfterRestore.build().get(METADATA_PREFIX + KEY_GROUP), basicHeader[1].getValue());
+        } finally {
+            FileUtils.deleteDirectory(tempDirectory.toFile());
+        }
     }
 
 
@@ -103,6 +121,9 @@ public class WindowsMetadataRestore_Test {
             final Class aClass = WindowsMetadataStore.class;
             final Method method = aClass.getDeclaredMethod("saveFlagMetaData", new Class[]{Path.class});
             method.setAccessible(true);
+
+            final File file = ResourceUtils.loadFileResource(FILE_NAME).toFile();
+
             method.invoke(windowsMetadataStore, file.toPath());
 
             final Method methodWindowsfilePermissions = aClass.getDeclaredMethod("saveWindowsfilePermissions", new Class[]{Path.class});
@@ -130,7 +151,6 @@ public class WindowsMetadataRestore_Test {
 
 
     private Metadata genMetadata(final Header... headers) {
-
         final ImmutableMultimap.Builder<String, String> mapBuilder = ImmutableMultimap.builder();
         for (final Header header : headers) {
             mapBuilder.put(header.getName(), header.getValue());
