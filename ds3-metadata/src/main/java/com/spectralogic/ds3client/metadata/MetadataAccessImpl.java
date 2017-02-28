9bd3e5874a00f1fe1f63b2a940dc3581ece8ffb2
GraciesPadre <ShastasPadre1@gmail.com>
2017-02-27 17:19:54 -0700

    Making the MetadataAccessImpl class emit a failure event, instead of throwing an exception, because the Windoze record and restore doesn't work if you don't own the files it is trying to access or if you are not running as admin.

--- ds3-metadata/src/main/java/com/spectralogic/ds3client/metadata/MetadataAccessImpl.java
+++ ds3-metadata/src/main/java/com/spectralogic/ds3client/metadata/MetadataAccessImpl.java
@@ -17,8 +17,11 @@ package com.spectralogic.ds3client.metadata;
 
 
 import com.google.common.collect.ImmutableMap;
+import com.spectralogic.ds3client.helpers.FailureEventListener;
 import com.spectralogic.ds3client.helpers.MetadataAccess;
+import com.spectralogic.ds3client.helpers.events.FailureEvent;
 import com.spectralogic.ds3client.metadata.interfaces.MetadataStore;
+import com.spectralogic.ds3client.utils.Guard;
 import com.spectralogic.ds3client.utils.Platform;
 
 import java.io.IOException;
@@ -28,15 +31,31 @@ import java.nio.file.attribute.BasicFileAttributes;
 import java.nio.file.attribute.PosixFileAttributes;
 import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * Implementation of MetaDataAcess Interface
  * Used to store meta data on Server
  */
 public class MetadataAccessImpl implements MetadataAccess {
+    private static final Logger LOG = LoggerFactory.getLogger(MetadataAccessImpl.class);
+
     private final ImmutableMap<String, Path> fileMapper;
+    private final FailureEventListener failureEventListener;
+    private final String httpEndpoint;
 
     public MetadataAccessImpl(final ImmutableMap<String, Path> fileMapper) {
+        this(fileMapper, null, null);
+    }
+
+    public MetadataAccessImpl(final ImmutableMap<String, Path> fileMapper,
+                              final FailureEventListener failureEventListener,
+                              final String httpEndpoint)
+    {
         this.fileMapper = fileMapper;
+        this.failureEventListener = failureEventListener;
+        this.httpEndpoint = httpEndpoint;
     }
 
     @Override
@@ -45,8 +64,26 @@ public class MetadataAccessImpl implements MetadataAccess {
         try {
             return storeMetaData(file);
         } catch (final IOException e) {
-            throw new RuntimeException("Error recording metadata.", e);
+            LOG.error("Error recording metadata.", e);
+            if (failureEventListener != null) {
+                failureEventListener.onFailure(FailureEvent.builder()
+                        .withObjectNamed(filename)
+                        .withCausalException(e)
+                        .doingWhat(FailureEvent.FailureActivity.RecordingMetadata)
+                        .usingSystemWithEndpoint(getHttpEndpointOrEmptyName())
+                        .build());
+            }
         }
+
+        return ImmutableMap.of();
+    }
+
+    private String getHttpEndpointOrEmptyName() {
+        if (Guard.isStringNullOrEmpty(httpEndpoint)) {
+            return " ";
+        }
+
+        return httpEndpoint;
     }
 
     /**
