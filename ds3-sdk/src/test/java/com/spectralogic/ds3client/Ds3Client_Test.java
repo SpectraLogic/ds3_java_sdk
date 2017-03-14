ce3659a1171db235278093314d6c2c71b2834c1c
GraciesPadre <ShastasPadre1@gmail.com>
2017-03-14 11:42:48 -0600

    Putting the sdk version in the default user agent header value

--- ds3-sdk/src/test/java/com/spectralogic/ds3client/Ds3Client_Test.java
+++ ds3-sdk/src/test/java/com/spectralogic/ds3client/Ds3Client_Test.java
@@ -44,6 +44,8 @@ import java.nio.file.StandardOpenOption;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import static org.hamcrest.CoreMatchers.*;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -903,6 +905,33 @@ public class Ds3Client_Test {
         assertThat(newClient.getConnectionDetails().getCredentials().getClientId(), is("access"));
         assertThat(newClient.getConnectionDetails().getCredentials().getKey(), is("key"));
         assertThat(newClient.getConnectionDetails().getUserAgent(), is(userAgent));
+    }
+
+    @Test
+    public void testGettingDefaultUserAgent() {
+        final Ds3Client client = Ds3ClientBuilder.create("endpoint", new Credentials("access", "key"))
+                .build();
+
+        final JobNode node = new JobNode();
+        node.setEndPoint("newEndpoint");
+        node.setHttpPort(80);
+        node.setHttpsPort(443);
+
+        final Ds3Client newClient = client.newForNode(node);
+        assertThat(newClient.getConnectionDetails().getEndpoint(), is("newEndpoint:443"));
+        assertThat(newClient.getConnectionDetails().getCredentials().getClientId(), is("access"));
+        assertThat(newClient.getConnectionDetails().getCredentials().getKey(), is("key"));
+
+        final String userAgent = newClient.getConnectionDetails().getUserAgent();
+        final String[] userAgentFields = userAgent.split("-");
+
+        assertThat(userAgentFields.length, is(2));
+
+        // look for a pattern like 3.4.0, but leave open the possibility of a string like 3.4.0-SNAPSHOT
+        final Pattern matchPattern = Pattern.compile("\\d+\\.\\d+\\.\\d+");
+        final Matcher matcher = matchPattern.matcher(userAgentFields[1]);
+
+        assertThat(matcher.find(), is(true));
     }
 
     @Test
