6a005136381958812a761f1982074d61f16b6b75
Rachel Tucker <rachelt@spectralogic.com>
2017-06-27 14:22:06 -0600

    Updated tests in UsersAndGroups to reflect change in 4.0.0 api

--- ds3-sdk-integration/src/main/java/com/spectralogic/ds3client/integration/Util.java
+++ ds3-sdk-integration/src/main/java/com/spectralogic/ds3client/integration/Util.java
@@ -65,10 +65,10 @@ public class Util {
         assumeThat(majorVersion, is(1));
     }
 
-    public static void assumeVersion3orLess(final Ds3Client client) throws IOException {
+    public static void assumeVersion4orHigher(final Ds3Client client) throws IOException {
         final int majorVersion = Integer.parseInt(client.getSystemInformationSpectraS3(
                 new GetSystemInformationSpectraS3Request()).getSystemInformationResult().getBuildInformation().getVersion().split("\\.")[0]);
-        assumeTrue(majorVersion <= 3);
+        assumeTrue(majorVersion >= 4);
     }
 
     public static void loadBookTestData(final Ds3Client client, final String bucketName) throws IOException, URISyntaxException {
