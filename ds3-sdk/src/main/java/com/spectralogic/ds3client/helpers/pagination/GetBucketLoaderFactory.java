4d15253da4349d16d599446a0cc7833efa56837d
Eric Bergstrom <bandwidthoracle@gmail.com>
2017-08-07 10:54:15 -0600

    Making sure methods remain the same

---
+++ ds3-sdk/src/main/java/com/spectralogic/ds3client/helpers/pagination/GetBucketLoaderFactory.java
@@ -0,0 +1,32 @@
+/*
+ * ******************************************************************************
+ *   Copyright 2014-2017 Spectra Logic Corporation. All Rights Reserved.
+ *   Licensed under the Apache License, Version 2.0 (the "License"). You may not use
+ *   this file except in compliance with the License. A copy of the License is located at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   or in the "license" file accompanying this file.
+ *   This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ *   CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ *   specific language governing permissions and limitations under the License.
+ * ****************************************************************************
+ */
+
+package com.spectralogic.ds3client.helpers.pagination;
+
+import com.spectralogic.ds3client.Ds3Client;
+import com.spectralogic.ds3client.models.Contents;
+import com.spectralogic.ds3client.utils.collections.LazyIterable;
+
+public class GetBucketLoaderFactory extends GetBucketKeyLoaderFactory<Contents> {
+
+    public GetBucketLoaderFactory(final Ds3Client client, final String bucket, final String keyPrefix, final String nextMarker, final int maxKeys, final int defaultListObjectsRetries) {
+        super(client, bucket, keyPrefix, "/" , nextMarker, maxKeys, defaultListObjectsRetries, contentsFunction);
+    }
+
+    @Override
+    public LazyIterable.LazyLoader<Contents> create() {
+        return super.create();
+    }
+}
